f7d40484ee45146a392841bf990e42b7045d1308
Marcio <marciopereiraaraujo1997d1@gmail.com>
2025-08-22 21:57:47 -0300

    fix: deploy

--- src/app/(candidato)/candidato-registro/formacao-academica/page.tsx
+++ src/app/(candidato)/candidato-registro/formacao-academica/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { AcedemicDataSchema } from '@/validation/candidateRegister/AcademicData';
-
 const AcademicBackgroundPage: React.FC = () => {
   return <h1>fomração academica</h1>;
 };
-export default AcedemicDataSchema;
+export default AcademicBackgroundPage;
