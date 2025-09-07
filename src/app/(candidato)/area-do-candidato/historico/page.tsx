80f2013fe7301567aca04fb402c64177d1508b55
Marcio <marciopereiraaraujo1997d1@gmail.com>
2025-09-06 21:24:51 -0300

    feat: history page

---
+++ src/app/(candidato)/area-do-candidato/historico/page.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import VacancyCard from '@/components/VacancyCard/VacancyCard';
+import SearchBarComponent from '@/components/Inputs/SearchBar';
+import { useState } from 'react';
+import { CardsContainer, Page, TitleSection } from './styles';
+
+interface IVancancyCardProps {
+  title: string;
+  description: string;
+  location: string;
+  salary: string;
+}
+
+const mockedvacancies: IVancancyCardProps[] = Array.from(
+  { length: 20 },
+  (_, index) => ({
+    title: `Vaga ${index + 1}`,
+    description:
+      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore inventore odio, autem suscipit obcaecati voluptates nulla mollitia reiciendis molestiae magni illo perferendis iure impedit nam alias natus culpa tempora ab.',
+    location: 'São Paulo, SP',
+    salary: 'R$ 3.000,00',
+  }),
+);
+const HistoryPage: React.FC = () => {
+  const [search, setSearch] = useState<string>('');
+  return (
+    <Page>
+      <TitleSection>
+        <div>
+          <h1>Histórico</h1>
+          <p>Veja as vagas que você se candidatou</p>
+        </div>
+        <SearchBarComponent
+          value={search}
+          placeholder="Pesquisar vaga"
+          onSearch={e => setSearch(e.target.value)}
+        />
+      </TitleSection>
+      <CardsContainer>
+        {mockedvacancies.map((vacancy, index) => (
+          <VacancyCard
+            key={index}
+            title={vacancy.title}
+            description={vacancy.description}
+            location={vacancy.location}
+            salary={vacancy.salary}
+          />
+        ))}
+      </CardsContainer>
+    </Page>
+  );
+};
+export default HistoryPage;
---
+++ src/app/(candidato)/area-do-candidato/historico/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Page = styled.div`
+  margin: 0 auto;
+`;
+
+export const TitleSection = styled.div`
+  position: sticky;
+  z-index: 999;
+  top: 0;
+  background: white;
+  height: 8rem;
+  padding: 0 2rem;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+  margin-bottom: 3.2rem;
+
+  h1 {
+    font-size: 3.2rem;
+    color: ${({ theme }) => theme.colors.GRAY.hex_353535};
+  }
+
+  p {
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.colors.GRAY.hex_747474};
+  }
+`;
+
+export const CardsContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-bottom: 4rem;
+`;
