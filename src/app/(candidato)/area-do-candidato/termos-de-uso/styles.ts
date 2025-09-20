d0d6f31f277311e11102ef7bc1befbc90f90fe76
Marcio <marciopereiraaraujo1997d1@gmail.com>
2025-09-20 17:14:51 -0300

    feat(candidate): terms of use page

---
+++ src/app/(candidato)/area-do-candidato/termos-de-uso/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  padding: 3.2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  width: clamp(30rem, 90%, 100rem);
+  margin: 0 auto;
+
+  font-size: 1.6rem;
+`;
+export const Paragraph = styled.p`
+  text-align: justify;
+  line-height: 2.4rem;
+  text-indent: 1.6rem;
+`;
+
+export const Ul = styled.ul`
+  list-style-type: none;
+`;
