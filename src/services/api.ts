ce9ec652c88633820612e11619b208c7deacd5f1
Marcio <marciopereiraaraujo1997d1@gmail.com>
2025-10-11 17:55:08 -0300

    fix: interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,7 @@
-/* eslint-disable dot-notation */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import axios, { AxiosRequestConfig } from 'axios';
 import { LOCAL_STORAGE_KEYS } from '@/utils/localStorageKeys';
 import { SESSION_STORAGE_KEYS } from '@/utils/sessionStorageKeys';
-import {
-  CandidatePublicRoutes,
-  EmployeePublicRoutes,
-} from '@/utils/routes/routes';
 
 export const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -32,22 +27,12 @@ const clearSessionStorage = () => {
 };
 
 const PUBLIC_ENDPOINTS = [
-  CandidatePublicRoutes.LOGIN,
-  CandidatePublicRoutes.EMAIL_VERIFICATION,
-  CandidatePublicRoutes.CODE_VERIFICATION,
-  CandidatePublicRoutes.PASSWORD_RESET,
-
-  CandidatePublicRoutes.REGISTER.STEP1,
-  CandidatePublicRoutes.REGISTER.STEP1_EMAIL,
-  CandidatePublicRoutes.REGISTER.STEP2,
-  CandidatePublicRoutes.REGISTER.STEP3,
-  CandidatePublicRoutes.REGISTER.STEP4,
-  CandidatePublicRoutes.REGISTER.STEP5,
-
-  EmployeePublicRoutes.LOGIN,
-  EmployeePublicRoutes.EMAIL_VERIFICATION,
-  EmployeePublicRoutes.CODE_VERIFICATION,
-  EmployeePublicRoutes.PASSWORD_RESET,
+  '/auth/login',
+  '/auth/logincandidato',
+  '/auth/refresh',
+  '/confirmaremail',
+  '/confirmaremail/validar',
+  '/candidato/aceitartermos',
 ];
 
 api.interceptors.request.use(
@@ -55,15 +40,17 @@ api.interceptors.request.use(
     const isPublicEndpoint = PUBLIC_ENDPOINTS.some(endpoint =>
       config.url?.startsWith(endpoint),
     );
+    console.log({ isPublicEndpoint });
     if (!isPublicEndpoint) {
       const token =
         sessionStorage.getItem(SESSION_STORAGE_KEYS.token) ||
         localStorage.getItem(LOCAL_STORAGE_KEYS.token);
 
+      console.log('Using token for request:', token);
       if (token) {
         if (config.headers) {
           config.headers = config.headers || {};
-          config.headers['Authorization'] = `Bearer ${JSON.parse(token)}`;
+          config.headers.Authorization = `Bearer ${JSON.parse(token)}`;
         }
       }
     }
@@ -162,7 +149,7 @@ api.interceptors.response.use(
             // Once the refresh is done, a new promise is created and fulfilled
             // with the new token. The original request is retried with the new token.
             if (originalRequest.headers) {
-              originalRequest.headers['Authorization'] = `Bearer ${token}`;
+              originalRequest.headers.Authorization = `Bearer ${token}`;
             }
             return api(originalRequest);
           })
@@ -181,9 +168,7 @@ api.interceptors.response.use(
         if (newAccessToken) {
           // If succsss, update the token in the headers for the original request.
           if (originalRequest.headers) {
-            originalRequest.headers[
-              'Authorization'
-            ] = `Bearer ${newAccessToken}`;
+            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
           }
           // Process all the requests in the queue.
           processQueue(null, newAccessToken);
