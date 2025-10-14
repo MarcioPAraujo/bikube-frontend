45991f1572e753334411953f31a8b300420d0c10
Marcio <marciopereiraaraujo1997d1@gmail.com>
2025-10-14 20:19:41 -0300

    feat(paySlip): generate pdf function

--- src/app/(funcionario)/home/page.tsx
+++ src/app/(funcionario)/home/page.tsx
@@ -1,15 +1,49 @@
 'use client';
 
+import { api } from '@/services/api';
+
 const HomePage = () => {
+  const handleGeneratePdf = async () => {
+    const endpoint = '/espelho/gerarPDF';
+
+    const body = {
+      idespelho: 1,
+    };
+
+    try {
+      const response = await api.post(endpoint, body, {
+        responseType: 'blob', // Important for binary data
+      });
+
+      // Create a URL for the blob
+      const url = window.URL.createObjectURL(
+        new Blob([response.data], { type: 'application/pdf' }),
+      );
+
+      // Create a link element
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'espelho.pdf'); // Set the file name
+
+      // Append to the document and trigger the download
+      document.body.appendChild(link);
+      link.click();
+
+      // Clean up and remove the link
+      link.parentNode?.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error('Error generating PDF:', error);
+    }
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
-      <select id="exampleSelect" name="exampleSelect">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </select>
+      <button type="button" onClick={handleGeneratePdf}>
+        gerar pdf
+      </button>
     </div>
   );
 };
---
+++ src/services/paySlip/paySlipService.ts
@@ -0,0 +1,28 @@
+import { Result } from '@/interfaces/apiResult';
+import handleError from '@/utils/handleError';
+import { api } from '../api';
+
+export const generatePaySlipPdf = async (
+  id: number,
+): Promise<Result<string>> => {
+  const endpoint = '/espelho/gerarPDF';
+
+  const body = {
+    idespelho: id,
+  };
+
+  try {
+    const response = await api.post(endpoint, body, {
+      responseType: 'blob', // Important for binary data
+    });
+
+    // Create a URL for the blob
+    const url = window.URL.createObjectURL(
+      new Blob([response.data], { type: 'application/pdf' }),
+    );
+
+    return { error: null, data: url };
+  } catch (error) {
+    return handleError(error, 'Erro ao gerar PDF do espelho de pagamento.');
+  }
+};
